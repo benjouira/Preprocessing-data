c92858fb29f25c6b5dac1740af29d65ad8bc3e4d
Rabii <rabi3jouira@gmail.com>
2022-01-23 19:57:58 +0100

    Create Preprocessing_data.py

---
+++ Preprocessing_data.py
@@ -0,0 +1,14 @@
+from sklearn import preprocessing
+import numpy as np
+X_train = np.array([[ 1., -1.,  2.],
+                    [ 2.,  0.,  0.],
+                    [ 0.,  1., -1.]])
+scaler = preprocessing.StandardScaler().fit(X_train)
+print(scaler)
+
+scaler.mean_
+
+scaler.scale_
+
+X_scaled = scaler.transform(X_train)
+X_scaled
