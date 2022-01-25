9d833ecaf209d93e02dcdb27698efac6faf1b82a
Rabii <rabi3jouira@gmail.com>
2022-01-25 20:30:01 +0100

    Update StandardScaler.py

--- StandardScaler.py
+++ StandardScaler.py
@@ -1,3 +1,8 @@
 from sklearn.preprocessing import StandardScaler
 std = StandardScaler()
 X = std.fit_transform(df[['Age','Weight']])
+
+
+from sklearn.preprocessing import StandardScaler
+std = StandardScaler()
+X = std.fit_transform(df[['Age','Weight']])
